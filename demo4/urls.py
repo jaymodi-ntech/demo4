3513aaea387bc736397b419628ce1bf3f76ade9c
ntech <ntech@n-tech.(none)>
2014-10-06 18:59:24 +0530

    update in serializers

--- demo4/urls.py
+++ demo4/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from snippet import urls
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'demo4.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('snippet.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
