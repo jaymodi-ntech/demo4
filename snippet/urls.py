5ceefc1b01089974e06a4bf5c604785e9e391e71
ntech <ntech@n-tech.(none)>
2014-10-07 10:55:26 +0530

    url to get,update,delete data

--- snippet/urls.py
+++ snippet/urls.py
@@ -1,3 +1,8 @@
 __author__ = 'mj'
+from django.conf.urls import patterns, url
 
+urlpatterns = patterns('snippet.views',
+    url(r'^snippets/$', 'snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+)
 
