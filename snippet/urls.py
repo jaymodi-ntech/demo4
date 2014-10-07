4bbfa4415cc9edad3a2a08a5f44ecabe57b88633
ntech <ntech@n-tech.(none)>
2014-10-07 12:03:18 +0530

    format suffix added

--- snippet/urls.py
+++ snippet/urls.py
@@ -1,8 +1,11 @@
 __author__ = 'mj'
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('snippet.views',
     url(r'^snippets/$', 'snippet_list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
 )
 
+urlpatterns = format_suffix_patterns(urlpatterns)
+# This will enable working of the suffix like .json and .yaml etc
